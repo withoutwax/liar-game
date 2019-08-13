ff8855b918888a27133dd24d563760d2478b4813
Will <rlagmlckd@gmail.com>
2019-08-13 01:26:53 -0500

    Updates

---
+++ precache-manifest.90ce30ccc16dd0b654eed6df4068870f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336e8f608a07204a9187a24de5fcb376",
+    "url": "/withoutwax/Liar-Game/index.html"
+  },
+  {
+    "revision": "2138d30664a246aea732",
+    "url": "/withoutwax/Liar-Game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "147d462629fec3535ada",
+    "url": "/withoutwax/Liar-Game/static/js/2.654589e8.chunk.js"
+  },
+  {
+    "revision": "2138d30664a246aea732",
+    "url": "/withoutwax/Liar-Game/static/js/main.9a7fe1e6.chunk.js"
+  },
+  {
+    "revision": "af0699d808e8c4bf0e5d",
+    "url": "/withoutwax/Liar-Game/static/js/runtime~main.e14bae6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/withoutwax/Liar-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
