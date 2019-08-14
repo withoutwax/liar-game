e82974323752d6124a8394910dcb8888ef6b8491
Will <rlagmlckd@gmail.com>
2019-08-14 01:37:48 -0500

    Implementing Introduction Page
    - Added Intro Page & button navigation with React Hooks.

---
+++ src/Components/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Intro() {
+    return (
+        <section>
+            <h1>Liar Game</h1>
+            <p>누가 거짓말을 하고 있을까요?</p>
+        </section>
+    );
+}
+
+export default Intro;
---
+++ src/Components/Setting.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Setting extends React.Component {
+    render() {
+        return <h1>설정 창</h1>
+    }
+}
+
+export default Setting;
