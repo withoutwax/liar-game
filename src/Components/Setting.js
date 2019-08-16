8839b80bb002668cb3fcef91e2d2fcfb72760d1a
Will <rlagmlckd@gmail.com>
2019-08-15 23:49:50 -0500

    Implementing basic visual for the settings menu
    - Added three options for the users in the settings:
    — 참여인원
    — 스파이 포함 여부
    — 게임 주제

--- src/Components/Setting.js
+++ src/Components/Setting.js
@@ -2,10 +2,69 @@ import React from 'react';
 import '../scss/Setting.scss';
 
 class Setting extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            playerNum: "3",
+            spyMode: false,
+            theme: ""
+        }
+    }
+
+    selectMenuChange = (event) => {
+        this.setState({playerNum: event.target.value});
+        console.log(event.target.value, this.state.playerNum);
+    }
+    spyModeSelect = (event) => {
+        // console.log(this.state.spyMode);
+        console.log(event.target.type);
+        let value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
+
+        this.setState({
+            spyMode: value
+        });
+    }
+    setTheme = (event) => {
+        console.log(event.target.value);
+        this.setState({theme: event.target.value});
+    }
+
     render() {
         return (
             <section className="setting-container">
                 <h1>설정 창</h1>
+
+                <form>
+                    <label>
+                        참여인원: {this.state.playerNum}
+                        <select value={this.state.value} onChange={this.selectMenuChange}>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
+                    </label>
+                    <label>
+                        {`스파이 모드: ${this.state.spyMode}`}
+                        <input 
+                            name="spyMode"
+                            type="checkbox"
+                            checked={this.state.spyMode}
+                            onChange={this.spyModeSelect}
+                        />
+                    </label>
+                </form>
+
+                <div>
+                    <h2>주제: {`${this.state.theme}`}</h2>
+                    <button value="food" onClick={this.setTheme}>음식</button>
+                    <button value="place" onClick={this.setTheme}>장소</button>
+                    <button value="occupation" onClick={this.setTheme}>직업</button>
+                </div>
             </section>
             );
     }
